0ce4691ce764d0b1f28d26cb73d38c80bb46013d
= <=>
2017-10-07 11:32:09 +0800

    更新交互型测试各种功能的函数

--- InteractTesing.kt
+++ InteractTesing.kt
@@ -4,39 +4,58 @@
 //其中输入\q有返回上一层功能,可以进行测试新的正则表达式/切换测试函数
 fun main(args: Array<String>) {
     while (true) {
-        println("\nplease choose which function to test..")
-        println("1.match 2.findAll 3.search 4.replace (\\q to quit)")
+        println("\n选择需要测试函数的序号..")
+        println("1.match 2.findAll 3.search \n4.replace 5.Match.group 6.Match.groups() (输入\\q to quit)")
         var testMod  = readLine()!!
         if (testMod == "\\q")
             break
 
         while (true) {
-            println("\nInput a new regex to compile..then test (\\b to test another func)")
+            println("输入一个新正则表达式进行不断测试..(\\b to 测试另一个函数)")
             val re = readLine()!!
             if (re == "\\b")
                 break
-            val compiled = compile(re)
-            println("regex compile Done!\n")
 
-            println("try a str to test if match this compiled regex..(\\b to test another regex)")
+            println("输入一个字符串去匹配这个正则表达式(\\b to 测试另一个表达式)")
             var text: String? = readLine()
 
             while (text != "\\b") {
 
-                when(testMod){
-                    "1" -> {println(compiled.match(text))
-                        println(compiled.match(text)!!.groups())}
-                    "2" -> println(compiled.findAll(text))
-                    "3" -> println(compiled.search(text))
-                    "4" ->{
-                        println("input the replacement")
-                        var replacement = readLine()!!
-                        println(compiled.replace(replacement, text))
+                when(testMod) {
+                    "1" -> println(match(re, text))
+                    "2" -> println(findAll(re, text))
+                    "3" -> println(search(re, text))
+                    "4" -> {
+                        println("输入匹配部分替换成的字符串")
+                        val replacement = readLine()!!
+                        println(replace(re, replacement, text))
                     }
-
+                    "5" -> {
+                        val match = match(re, text)
+                        if (match != null) {
+                            println("匹配结果:")
+                            println(match)
+                            println("输入要group捕获的组别编号")
+                            val groupId = readLine()!!.toInt()
+                            println(match.group(groupId))
+                        }
+                        else
+                            print("不匹配正则表达式!")
+                    }
+                    "6" -> {
+                        val match = match(re, text)
+                        if (match != null) {
+                            println("匹配结果:")
+                            println(match)
+                            println("所有捕获的组别:")
+                            println(match.groups())
+                        }
+                        else
+                            print("不匹配正则表达式!")
+                    }
                 }
 
-                println("try another str to test..(\\b to test another regex)")
+                println("输入另个字符串去匹配这个正则表达式(\\b to 测试另一个表达式)")
                 text = readLine()
 
             }
